73d8862d3493198be688c7fe247f06f84778a7d7
Nicholas Watson <nwwatson@gmail.com>
2012-08-29 00:56:45 -0400

    added needed css an js to manifest files

--- vendor/assets/javascripts/pushpop_rails.js
+++ vendor/assets/javascripts/pushpop_rails.js
@@ -1,4 +1,6 @@
 //= require externals/scrollkit/scrollkit
 //= require pushpop
-//= require pushpop-table-view2/pushpop-table-view
+//= require pushpop-table-view/pushpop-table-view
 //= require pushpop-navigationbar/pushpop-navigationbar
+//= require pushpop-split-view/pushpop-split-view
+//= require pushpop-modal-view-stack/pushpop-modal-view-stack
